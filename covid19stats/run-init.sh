1053d1b688a76263a06364aadba20cf93988867c
Aveek Roy <aveekroy@Aveeks-MacBook-Pro.local>
2020-03-22 13:14:11 -0700

    Added shell script

--- covid19stats/run-init.sh
+++ covid19stats/run-init.sh
@@ -1,4 +1,5 @@
 git clone https://github.com/CSSEGISandData/COVID-19.git &&
-cd COVID-19/csse_covid_19_data/csse_covid_19_daily_report &&
-clear
+cd COVID-19/csse_covid_19_data/csse_covid_19_daily_reports &&
+clear &&
+
 
