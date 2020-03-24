0f7c0cc6389b46e379f082c387762132f87633f6
Aveek Roy <aveekroy@Aveeks-MacBook-Pro.local>
2020-03-24 05:17:21 -0700

    Modified the script

--- covid19stats/run-init.sh
+++ covid19stats/run-init.sh
@@ -1,6 +1,7 @@
 git clone https://github.com/CSSEGISandData/COVID-19.git &&
 cd /root/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports &&
 curl -s "https://raw.githubusercontent.com/aveekroy/covid19-scenario/master/covid19stats/assets/covid19_cases_details.sh" --output covid19_cases_details &&
+chmod 755 covid19_cases_details &&
 clear &&
 PS1="\` echo \[\e[32m\]^_^\[\e[0m\]\`\[\e[33m\][covid19-case-details]\e[0m\]\\$ " &&
 clear
