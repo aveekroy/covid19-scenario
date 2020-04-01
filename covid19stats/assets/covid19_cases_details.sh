bede864b962a9f89bd3b07494839d96973f795a9
Aveek Roy <aveekroy@Aveeks-MacBook-Pro.local>
2020-03-31 20:37:43 -0700

    modified script

--- covid19stats/assets/covid19_cases_details.sh
+++ covid19stats/assets/covid19_cases_details.sh
@@ -8,67 +8,36 @@ search=`echo Unassigned, ${state_us}`
 echo -e '\n'
 echo "Please find the details for \"${county_state_country}\""
 echo -e '\n'
-grep -rnw '/root/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports' -e "${county_state_country}"|sort|cut -d':' -f3-7 &> detailed_list_us
-grep -rnw '/root/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports' -e "${search}"|sort|cut -d':' -f3-7 >> detailed_list_us
-grep -rnw '/root/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports' -e "${county_state_country}"|sort|cut -d':' -f3-7 &> detailed_list
 
-if [ ${country} == "US" ];then
-cat detailed_list_us
+function latest_count(){
+                       
+    file=`ls -l|tail -3|head -1|awk -F' ' '{print $9}'`
+    echo -e "Details on $file for state ${state_us}\n"
+    cat $file|grep -w ${state_us} &> total
+
+    for i in `cat total|grep ${state_us}|awk -F ',' '{print $8}'`
+    do
+        infected=`expr $infected + $i`
+    done
+    echo -e "Total number of persons infected in ${state_us} is $infected."
+
+    for i in `cat total|grep ${state_us}|awk -F ',' '{print $9}'`
+    do
+        deaths=`expr $deaths + $i`
+    done
+    echo -e "Total number of deaths in ${state_us} is $deaths."
+    rm total
+}
+
+if [ -z ${country} ]; then
+        grep -rnw '/root/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports' -e "${county_state_country}"|sort|cut -d':' -f3-7 &> detailed_list
+        cat detailed_list
+        rm detailed_list
+        latest_count
 else
-cat detailed_list
+        grep -rnw '/root/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports' -e "${county_state_country}"|sort|cut -d':' -f3-7 &> detailed_list_us
+        grep -rnw '/root/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports' -e "${search}"|sort|cut -d':' -f3-7 >> detailed_list_us
+        cat detailed_list_us
+        rm detailed_list_us
 fi
-rm detailed_list detailed_list_us
 echo -e '\n'
-
-if [ ${country} == "US" ];then
-file=`ls -l|tail -3|head -1|awk -F' ' '{print $9}'`
-echo -e "Details on $file for state ${state_us}\n"
-cat $file|grep ${state_us} > total
-
-for i in `cat total|grep ${state_us}|awk -F ',' '{print $8}'`
-do
-infected=`expr $infected + $i`
-done
-echo -e "Total number of persons infected in ${state_us} is $infected."
-
-for i in `cat total|grep ${state_us}|awk -F ',' '{print $9}'`
-do
-deaths=`expr $deaths + $i`
-done
-echo -e "Total number of deaths in ${state_us} is $deaths."
-
-
-for i in `cat total|grep ${state_us}|awk -F ',' '{print $10}'`
-do
-recovered=`expr $recovered + $i`
-done
-echo -e "Total number of recovered people in ${state_us} is $recovered."
-
-for i in `cat total|grep ${state_us}|awk -F ',' '{print $11}'`
-do
-active=`expr $active + $i`
-done
-echo -e "Total number of active people in ${state_us} is $active."
-fi
-
-
-#Before 23rd March
-# #!/bin/bash
-
-# date=`ls -l|awk -F' ' '{print $9}'|tail -3|head -1`
-
-# if [ "$date" >= "2020-03-23.csv" ];
-# #echo -e "Details for '$date':"
-# then
-#     echo "Enter your County/State/Country Name:"
-#     read county_state_country
-#     for i in `ls -l *.csv|awk -F' ' '{print $9}'`
-#     do
-#     cat $i|grep -w "${county_state_country}"|awk -F',' '{print $2" ----- "$3" ----- "$4" ----- "$5"---> Confirmed: "$8" ----- Deaths: "$9" ----- Recovered: "$10" ----- Active: "$11}'
-#     done
-# else 
-#     for i in `ls -l *.csv|awk -F' ' '{print $9}'`
-#     do
-#     cat $i|grep -w "${country}"|grep -w "${state}"|awk -F',' '{print $3"---> Confirmed: "$4" ----- Deaths: "$5" ----- Recovered: "$6}'
-#     done
-# fi
